c18f939acc089d2f60b302114c2fa38a5a5f2801
Carl Caesar <carljfcaesar@gmail.com>
2024-11-24 22:25:37 +0100

    Trying to fix maven deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
-    id("java")
+    id("java-library")
+    id("maven-publish")
+    signing
 }
 
-group = "app.wse"
+group = "com.javxa.wse-app"
+version = System.getenv()["VERSION"] ?: "0.0.1"
+
+java {
+//    withJavadocJar()
+//    withSourcesJar()
+}
 
 repositories {
     mavenCentral()
@@ -17,3 +25,33 @@ tasks.test {
     useJUnitPlatform()
 }
 
+// https://www.jetbrains.com/help/idea/add-a-gradle-library-to-the-maven-repository.html
+// https://central.sonatype.com/publishing
+
+publishing {
+    publications {
+
+
+        create<MavenPublication>("WseApp") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "Custom"
+            url = uri(layout.buildDirectory.dir("custom-maven"))
+        }
+    }
+}
+
+//Javadocs must be provided but not found in entries
+//Missing signature for file: wse-java-0.0.1.jar
+//Missing signature for file: wse-java-0.0.1.module
+//Missing signature for file: wse-java-0.0.1.pom
+//Sources must be provided but not found in entries
+//Developers information is missing
+//License information is missing
+//Project URL is not defined
+//Project description is missing
+//SCM URL is not defined
